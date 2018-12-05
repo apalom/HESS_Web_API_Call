a834f904c19ae91ed12bdde757dc8b3e5c4d968d
apalom <35828580+apalom@users.noreply.github.com>
2018-12-05 15:14:52 -0700

    session parsing slow

--- HESS_Analyze.py
+++ HESS_Analyze.py
@@ -28,7 +28,7 @@ data = dataRaw
 
 tST= timeit.default_timer()
 
-colNames = ['TIME', 'DAY', 'WEEKDAY', 'KWH', 'KWHadded', 'KVAH', 'KVA', 'KW', 'KVAR', 'PF','VRMSA', 'IRMSA', 'ANGLEA']
+colNames = ['TIME', 'DAY', 'WEEKDAY', 'KWH', 'KWHadded', 'SESSION', 'KVAH', 'KVA', 'KW', 'KVAR', 'PF','VRMSA', 'IRMSA', 'ANGLEA']
 
 data = pd.DataFrame(data, index=np.arange(len(dataRaw)), columns=colNames)
 
@@ -37,22 +37,36 @@ data.TIME = pd.to_datetime(data.TIME)
 days = np.zeros((len(data),2));
 energyAdded = np.zeros((len(data),1));
 allPF = np.zeros((len(data),1));
+seshCount = np.zeros((len(data),1));
+
 
 data.KVA = 3*(data.VRMSA*data.IRMSA)/1000;
 data.KW = 3*(data.VRMSA*data.IRMSA)*(np.cos(data.ANGLEA*np.pi/180))/1000;
 data.KVAR = 3*(data.VRMSA*data.IRMSA)*(np.sin(data.ANGLEA*np.pi/180))/1000;
+result = [];
 
 for idx, row in data.iterrows():
-    days[idx][0] = row.TIME.dayofyear
-    days[idx][1] = row.TIME.weekday()
+    
+    days[idx][0] = row.TIME.dayofyear;
+    days[idx][1] = row.TIME.weekday();
+    seshCount[idx] = count;
     if row.KW != 0:
         allPF[idx] = np.cos(np.arctan(row.KVAR/row.KW))
     if idx < (len(data)-1):
         energy = data.KWH[idx+1] - data.KWH[idx] 
         energyAdded[idx] = energy;
-        #if energy < 20: 
-        #    energyAdded[idx] = energy;
-        
+
+count = 1;
+for idx, row in data.iterrows():
+    data.SESSION[idx] = count;    
+    
+    if idx < len(data)-1:
+        if data.KWHadded[idx] < 1.0 and data.KWHadded[idx+1] > 1.0:            
+            result.append(str(idx) + ' ' + str(count) + ' ' + str(data.KWHadded[idx]) + ' ' + str(data.KWHadded[idx+1]))
+            print(idx)
+            count = count + 1;
+                      
+            
 data.DAY = days[:,0];
 #Return the day of the week represented by the date. Monday == 0 … Sunday == 6
 data.WEEKDAY = days[:,1];
@@ -64,27 +78,68 @@ dataHead = data.head(100)
 tEl = timeit.default_timer() - tST
 print('Analysis Time: {0:.4f} sec'.format(tEl))
 
-#%% Histogram 
+#%% Calculate Session Energy
 
-allEnergy = list(set(data.KWH))
-allEnergy.sort()
-seshKWH = np.zeros((len(allEnergy),1))
+tST= timeit.default_timer()
 
-for i in range(len(allEnergy)-1):
-    seshKWH[i] = allEnergy[i+1] - allEnergy[i]
+numSessions = int(np.max(data.SESSION));
+seshEnergy = np.zeros((numSessions,1))
+idx = 0;
 
+for sesh in range(1, numSessions):
+    
+    dfTemp = data.loc[data.SESSION == sesh];
+    if len(dfTemp) != 1:
+        seshKWH = dfTemp.iloc[len(dfTemp)-1].KWH - dfTemp.iloc[0].KWH;
+        
+        print(sesh, ': ', seshKWH, 'kWh' )
+        
+        seshEnergy[idx] = seshKWH;
+        idx += 1;
 
-#%% Plot seshKWH Histogram 
+seshEnergy = seshEnergy[np.where( seshEnergy > 0.5 )];
+
+tEl = timeit.default_timer() - tST
+print('Energy Session: {0:.4f} sec'.format(tEl))
+
+#%% Plot seshEnergy Histogram 
 
 import matplotlib.pyplot as plt
 
 #maxBin = np.ceil(np.max(seshKWH)) + 0.5;
+maxBin = 150;
+binEdges = np.arange(0, maxBin, 5)
+
+n, bins, patches = plt.hist(seshEnergy, bins=binEdges, density=True, rwidth=0.75, color='#607c8e')
+                            
+plt.xlabel('Energy (kWh)')
+#plt.xticks(np.arange(0,maxBin+1,1))
+plt.ylabel('Frequency')
+plt.title('Energy Per Session')
+
+
+#%% Calculate minute Energy 
+
+allEnergy = list(set(data.KWH))
+allEnergy.sort()
+minKWH = np.zeros((len(allEnergy),1))
+
+for i in range(len(allEnergy)-1):
+    minKWH[i] = allEnergy[i+1] - allEnergy[i]
+
+
+#%% Plot minKWH Histogram 
+
+import matplotlib.pyplot as plt
+
+#maxBin = np.ceil(np.max(seshKWH)) + 0.5;
+minKWH = minKWH[np.where( minKWH > 0.5 )];
 maxBin = 10;
 binEdges = np.arange(0,maxBin,0.5)
 
-n, bins, patches = plt.hist(seshKWH, bins=binEdges, density=True, rwidth=0.75, color='#607c8e')
+n, bins, patches = plt.hist(minKWH, bins=binEdges, density=True, rwidth=0.75, color='#607c8e')
                             
-plt.xlabel('Session Energy (kWh)')
+plt.xlabel('Energy (kWh)')
 plt.xticks(np.arange(0,maxBin+1,1))
 plt.ylabel('Frequency')
 plt.title('Energy Per Minute')
@@ -150,3 +205,6 @@ kWhNeedPerBusDay = milesPerDay * eff;
 
 #Assume 3 busses on route
 kWhNeedPerDay = 3 * kWhNeedPerBusDay;
+
+#%%
+
