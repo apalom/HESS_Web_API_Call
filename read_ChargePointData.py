1865da149cac27a73a6406eea43d5ffeb7185ab9
apalom <35828580+apalom@users.noreply.github.com>
2019-01-14 17:35:46 -0700

    CP lifetime session details

--- read_ChargePointData.py
+++ read_ChargePointData.py
@@ -17,9 +17,10 @@ import datetime
 #%% Import System Data
 
 # Raw Data
-path = 'PackSize-Session-Details-Meter-with-Summary-20181211.csv';
+#filePath = 'PackSize-Session-Details-Meter-with-Summary-20181211.csv';
+filePath = 'Session-Details-Summary-ALL-Lifetime.csv';
 # Import Data
-dataRaw = pd.read_csv(path);
+dataRaw = pd.read_csv(filePath);
 data = dataRaw;
 dataHead = data.head(100);
 dataTypes = data.dtypes;
@@ -49,20 +50,18 @@ data['Charging Time (hh:mm:ss)'] = pd.to_timedelta(data['Charging Time (hh:mm:ss
 #%% df Energy
 
 dfEnergy = data.loc[data['Energy (kWh)'].notna()]
-dfEnergy = pd.DataFrame(dfEnergy, columns=['EVSE ID', 'Port', 'Station Name', 'Plug In Event Id', 'Power Start Time', 'Start Time',  'End Time',  'Total Duration (hh:mm:ss)', 'Duration (h)', 'Charging Time (hh:mm:ss)', 'Charging (h)', 'Energy (kWh)',  'Gasoline Savings', 'Port Type',  'Address 1',  'City',  'State/Province',  'Postal Code',  'Country',  'Latitude',  'Longitude',  'Ended By',  'Driver Postal Code'] ) 
+dfEnergy = pd.DataFrame(dfEnergy, columns=['EVSE ID', 'Station Name', 'Plug In Event Id', 'Total Duration (hh:mm:ss)', 'Duration (h)', 'Charging Time (hh:mm:ss)', 'Charging (h)', 'Energy (kWh)',  'Port Type',  'Address 1',  'City',  'State/Province',  'Postal Code',  'Latitude',  'Longitude',  'Ended By',  'Driver Postal Code'] ) 
 dfEnergy = dfEnergy.reset_index(drop=True);
 
-dfEnergy['Duration (h)'] = dfEnergy['Total Duration (hh:mm:ss)'].apply(lambda x: x.seconds//3600)
-dfEnergy['Charging (h)'] = dfEnergy['Charging Time (hh:mm:ss)'].apply(lambda x: x.seconds//3600)
-
-
+dfEnergy['Duration (h)'] = dfEnergy['Total Duration (hh:mm:ss)'].apply(lambda x: x.seconds/3600)
+dfEnergy['Charging (h)'] = dfEnergy['Charging Time (hh:mm:ss)'].apply(lambda x: x.seconds/3600)
 
 #%% Plot Session Energy Histogram
 
-binEdges = np.arange(int(np.min(dfEnergy['Energy (kWh)'])), int(np.max(dfEnergy['Energy (kWh)'])), 1)
+binEdges = np.arange(int(np.min(dfEnergy['Energy (kWh)'])), int(np.max(dfEnergy['Energy (kWh)'])), 5)
 numBins = int(np.sqrt(len(dfEnergy)));
 
-n, bins, patches = plt.hist(dfEnergy['Energy (kWh)'], bins=binEdges, density=True, rwidth=1.0, color='#607c8e', edgecolor='white', linewidth=1.0);
+n, bins, patches = plt.hist(dfEnergy['Energy (kWh)'], bins=binEdges, density=True, rwidth=0.8, color='#607c8e', edgecolor='white', linewidth=1.0);
 
 plt.xlabel('Energy (kWh)')
 #plt.xticks(np.arange(minVal, maxVal, 5))
@@ -73,16 +72,29 @@ plt.title('Energy Per Session')
 
 #%% Plot Session Duration Histogram
 
-binEdges = np.arange(0, 24, 1)
-numBins = int(np.sqrt(len(dfEnergy)));
+qE_high = dfEnergy['Duration (h)'].quantile(0.9545); #remove 2 std dev outlier
+qE_low = dfEnergy['Duration (h)'].quantile(1-0.9545); #remove 2 std dev outlier
 
-plt.hist(dfEnergy['Duration (h)'], bins=binEdges, histtype='bar', density=True, rwidth=1.0, color='#607c8e', edgecolor='white', linewidth=1.0);
-plt.hist(dfEnergy['Charging (h)'], bins=binEdges, histtype='bar', density=True, alpha=0.6, rwidth=1, color='lightblue', edgecolor='white', linewidth=1.0);
+df_time = dfEnergy[(dfEnergy['Duration (h)'] > qE_low) & (dfEnergy['Duration (h)'] < qE_high)]
 
-plt.xlabel('Hours')
-plt.xticks(np.arange(0, 24, 2))
+df_time = pd.DataFrame(df_time, columns=['EVSE ID', 'Station Name', 'Plug In Event Id', 'Duration (h)', 'Duration (m)', 'Charging (h)', 'Charging (m)', 'Energy (kWh)'] ) 
+df_time = df_time.reset_index(drop=True)
+df_time['Duration (m)'] = df_time['Duration (h)'] * 60
+df_time['Charging (m)'] = df_time['Charging (h)'] * 60
+
+#%% 
+
+binEdges = np.arange(0, 1215, 15)
+#numBins = int(np.sqrt(len(df_time)));
+
+n1, bins1, patches1 = plt.hist(df_time['Duration (m)'], bins=binEdges, histtype='bar', density=True, rwidth=1.0, color='#607c8e', edgecolor='white', linewidth=1.0);
+n2, bins2, patches2 = plt.hist(df_time['Charging (m)'], bins=binEdges, histtype='bar', density=True, alpha=0.6, rwidth=1, color='lightblue', edgecolor='white', linewidth=1.0);
+
+plt.xlabel('Minutes')
+plt.xticks(np.arange(0, 1200, 120))
+plt.xlim([0,480])
 plt.ylabel('Frequency')
-plt.title('Session Duration')
+plt.title('ChargePoint Lifetime Session Duration')
 plt.legend(['Connected Time', 'Charging Time'])
 #plt.grid()
 
