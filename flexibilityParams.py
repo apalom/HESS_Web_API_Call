3b90705f7ec89c83de297b0f7b0c96c1053bc3f7
apalom <35828580+apalom@users.noreply.github.com>
2019-02-21 13:45:26 -0700

    Create flexibilityParams.py

---
+++ flexibilityParams.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 10:19:45 2019
+
+@author: Alex
+"""
+
+import numpy as np
+from scipy.stats import mstats
+import matplotlib.pyplot as plt
+import pandas as pd
+from os import path
+import timeit
+import time
+import datetime
+
+#%% Import System Data
+
+# Raw Data
+#filePath = 'PackSize-Session-Details-Meter-with-Summary-20181211.csv';
+filePath = 'data/Lifetime-Session-Details.csv';
+#filePath = 'data/Lifetime-UniqueDrivers-vs-Time.csv';
+
+# Import Data
+dataRaw = pd.read_csv(filePath);
+data = dataRaw;
+dataHead = data.head(100);
+dataTypes = data.dtypes;
+
+allColumns = list(data);
+
+#%% Data Columns for ChargePoint 'data/Lifetime-Session-Details.csv';
+
+colNames = ['EVSE ID', 'Port Number', 'Station Name', 'Plug In Event Id', 'Start Date', 'End Date', 
+            'Total Duration (hh:mm:ss)', 'Charging Time (hh:mm:ss)', 'Energy (kWh)',
+            'Ended By', 'Port Type', 'Latitude', 'Longitude', 'User ID', 'Driver Postal Code'];
+            
+data = pd.DataFrame(data, index=np.arange(len(dataRaw)), columns=colNames)
+
+data['Start Date'] = pd.to_datetime(data['Start Date']);
+data['End Date'] = pd.to_datetime(data['End Date']);
+data['Total Duration (hh:mm:ss)'] = pd.to_timedelta(data['Total Duration (hh:mm:ss)']);
+data['Charging Time (hh:mm:ss)'] = pd.to_timedelta(data['Charging Time (hh:mm:ss)']);
+
+dataHead = data.head(100);
+
+#%% Filter for Packsize
+
+dfPacksize = data[data['Station Name'].str.contains("PACKSIZE")]
+dfPacksize = dfPacksize.sort_values(by=['Start Date']);
+dfPacksize = dfPacksize.reset_index(drop=True);
+
+dfPacksize = dfPacksize.loc[data['Energy (kWh)'].notna()]
+dfPacksize['Duration (h)'] = dfPacksize['Total Duration (hh:mm:ss)'].apply(lambda x: x.seconds/3600)
+dfPacksize['Duration (h)'] = dfPacksize['Duration (h)'].apply(lambda x: round(x * 2) / 4) 
+dfPacksize['Charging (h)'] = dfPacksize['Charging Time (hh:mm:ss)'].apply(lambda x: x.seconds/3600)
+dfPacksize['Charging (h)'] = dfPacksize['Charging (h)'].apply(lambda x: round(x * 2) / 4) 
+
+dfPacksize['DayofYr'] = dfPacksize['Start Date'].apply(lambda x: x.dayofyear) 
+dfPacksize['DayofWk'] = dfPacksize['Start Date'].apply(lambda x: x.weekday()) 
+dfPacksize['StartHr'] = dfPacksize['Start Date'].apply(lambda x: x.hour + x.minute/60) 
+dfPacksize['StartHr'] = dfPacksize['StartHr'].apply(lambda x: round(x * 2) / 4) 
+
+#%% Flexibility Parameters
+
+
+
+
+i = 0;
+numBins = 10;
+connected = np.zeros((numBins,24*4))
+
+allEVSEs = list(set(dfPacksize['EVSE ID']));
+allHrs = list(set(dfPacksize['StartHr']));
+
+for EVSE in allEVSEs:
+    dfTemp = dfPacksize.loc[dfPacksize['EVSE ID'] == EVSE]  
+    
+    dfTemp1 = dfTemp.loc[dfTemp['Port Number'] == '1']    
+    
+    stdD = np.std(dfTemp1['StartHr'])
+    n = len(dfTemp1)
+    h = #Scotts Normal Reference rule for bin width (h) = 3.5*stdDev / n^(1/3)
+    
+    n1 = np.histogram(dfTemp1['StartHr'], density=True);
+    
+    dfTemp2 = dfTemp.loc[dfTemp['Port Number'] == '2']
+    n2 = np.histogram(dfTemp2['StartHr'], density=True);
+    
+    # --- Connected ---
+    i += 1;
+
+#%% df Energy
+
+col1 =  ['EVSE ID', 'Port Number', 'Station Name', 'Plug In Event Id', 'Start Date', 'End Date', 
+            'Total Duration (hh:mm:ss)', 'Duration (h)', 'Charging Time (hh:mm:ss)', 'Charging (h)', 
+            'Energy (kWh)', 'Ended By', 'Port Type', 'Latitude', 'Longitude', 'User ID', 'Driver Postal Code'];
+
+dfEnergy = dfPacksize.loc[data['Energy (kWh)'].notna()]
+dfEnergy = pd.DataFrame(dfEnergy, columns=col1)
+
+dfEnergy = dfEnergy.reset_index(drop=True);
+
+dfEnergy['Duration (h)'] = dfEnergy['Total Duration (hh:mm:ss)'].apply(lambda x: x.seconds/3600)
+dfEnergy['Charging (h)'] = dfEnergy['Charging Time (hh:mm:ss)'].apply(lambda x: x.seconds/3600)
+
+dfEnergy['DayofYr'] = dfEnergy['Start Date'].apply(lambda x: x.dayofyear) 
+dfEnergy['DayofWk'] = dfEnergy['Start Date'].apply(lambda x: x.weekday()) 
+dfEnergy['StartHr'] = dfEnergy['Start Date'].apply(lambda x: x.hour) 
+dfEnergy['StartHr'] = dfEnergy['StartHr'].apply(lambda x: np.round(x) if x < 23.5 else np.floor(x)) 
+
+#%% Plot Session Energy Histogram
+
+#binEdges = np.arange(int(np.min(dfEnergy['Energy (kWh)'])), int(np.max(dfEnergy['Energy (kWh)'])), 1)
+binEdges = np.arange(int(np.min(dfEnergy['Energy (kWh)'])), 30, 1)
+numBins = int(np.sqrt(len(dfEnergy)));
+
+n, bins, patches = plt.hist(dfEnergy['Energy (kWh)'], bins=binEdges, density=True, rwidth=1.0, color='#607c8e', edgecolor='white', linewidth=1.0);
+
+plt.xlabel('Energy (kWh)')
+#plt.xticks(np.arange(minVal, maxVal, 5))
+plt.ylabel('Frequency')
+plt.title('Energy Per Session')
+#plt.grid()
+
+print('Mean: ', np.mean(dfEnergy['Energy (kWh)']) , ' | StdDev: ', np.std(dfEnergy['Energy (kWh)']))
+
+
+#%% Calculate Session Duration Histogram
+
+qE_high = dfEnergy['Duration (h)'].quantile(0.9545); #remove 2 std dev outlier
+qE_low = dfEnergy['Duration (h)'].quantile(1-0.9545); #remove 2 std dev outlier
+
+df_time = dfEnergy[(dfEnergy['Duration (h)'] > qE_low) & (dfEnergy['Duration (h)'] < qE_high)]
+
+df_time = pd.DataFrame(df_time, columns=['EVSE ID', 'Station Name', 'Plug In Event Id', 'Duration (h)', 'Duration (m)', 'Charging (h)', 'Charging (m)', 'Energy (kWh)'] ) 
+df_time = df_time.reset_index(drop=True)
+df_time['Duration (m)'] = df_time['Duration (h)'] * 60
+df_time['Charging (m)'] = df_time['Charging (h)'] * 60
+
+#%% Plot Session Duration Histogram
+
+binEdges = np.arange(0, 12, 0.5)
+#numBins = int(np.sqrt(len(df_time)));
+
+#n1, bins1, patches1 = plt.hist(df_time['Duration (h)'], bins=binEdges, histtype='bar', density=True, rwidth=1.0, color='#607c8e', edgecolor='white', linewidth=1.0);
+#n2, bins2, patches2 = plt.hist(df_time['Charging (h)'], bins=binEdges, histtype='bar', density=True, alpha=0.6, rwidth=1, color='lightblue', edgecolor='white', linewidth=1.0);
+
+n1, bins1, patches1 = plt.hist(df_time['Duration (h)'], bins=binEdges, histtype='step', density=True, color='#607c8e', edgecolor='#607c8e', linewidth=1.0);
+n2, bins2, patches2 = plt.hist(df_time['Charging (h)'], bins=binEdges, histtype='step', density=True, alpha=0.6, color='lightblue', edgecolor='blue', linewidth=1.0);
+
+plt.xlabel('Hours')
+plt.xticks(np.arange(0, 13, 1))
+#plt.xlim([0,480])
+plt.ylabel('Frequency')
+plt.title('ChargePoint Lifetime Session Duration')
+plt.legend(['Connected Time', 'Charging Time'])
+#plt.grid()
+
+print('Connected - Mean: ', np.mean(df_time['Duration (h)']) , ' | StdDev: ', np.std(df_time['Duration (h)']) )
+print('Charging - Mean: ', np.mean(df_time['Charging (h)']) , ' | StdDev: ', np.std(df_time['Charging (h)']) )
+
+#%% EVSE Hogging (Sparrow)
+
+from datetime import timedelta
+
+allEvents = list(set(dfPacksize['Plug In Event Id']));
+
+i=0;
+sparrow = np.zeros((len(allEvents),2));
+
+for eventID in allEvents:
+    print(eventID)
+    dfTemp = dfPacksize.loc[dfPacksize['Plug In Event Id'] == eventID]  
+    
+    connectHr = dfTemp['Start Date'].iloc[0].hour
+    sparrow[i,0] = connectHr;
+    
+    connectTime = dfTemp['Total Duration (hh:mm:ss)'].iloc[0];
+    powerTime = dfTemp['Charging Time (hh:mm:ss)'].iloc[0];
+    if connectTime > timedelta(seconds = 0):
+        sparrow[i,1] = powerTime/connectTime;
+    i += 1;
+
+dfSparrow = pd.DataFrame(sparrow, columns=['PluginHour', 'Sparrow']);
+dfSparrow = dfSparrow.sort_values(by=['PluginHour', 'Sparrow']);
+dfSparrow = dfSparrow.reset_index(drop=True);
+    
+#%% Sparrow Histogram
+plt.style.use('default')
+
+plt.hist(dfSparrow.Sparrow, density=True, rwidth=1.0, color='#607c8e', edgecolor='white', linewidth=1.0);
+plt.title('Charger Utilization Efficiency')
+plt.xlabel('Sparrow Factor')
+plt.ylabel('Frequency')
+
+plt.show()
+
+#%% Sparrow Margin Plots
+
+import seaborn as sns
+from scipy import stats
+
+g = sns.jointplot(dfSparrow.PluginHour, dfSparrow.Sparrow, color='lightblue', kind='kde')
+#g = sns.jointplot(dfSparrow.PluginHour, dfSparrow.Sparrow, color='blue', alpha='0.05', kind='kde')
+
+g.ax_joint.set_xticks(np.arange(0,26,2))
+g.annotate(stats.pearsonr, loc=(1.2,1), fontsize=0.1)
+
+
+#%% Calculate Arrival Quartiles
+
+dfEnergy['DayofYr'] = dfEnergy['Start Date'].apply(lambda x: x.dayofyear) 
+dfEnergy['DayofWk'] = dfEnergy['Start Date'].apply(lambda x: x.weekday()) 
+dfEnergy['StartHr'] = dfEnergy['Start Date'].apply(lambda x: x.hour) 
+dfEnergy['StartHr'] = dfEnergy['StartHr'].apply(lambda x: np.round(x) if x < 23.5 else np.floor(x)) 
+
+i=0;
+days = list(set(dfEnergy['DayofYr']));
+
+arrivalCounts = np.zeros((len(days),24));
+arrivalPcts = np.zeros((len(days),24));
+hrs = np.arange(0,24);
+d=0;
+
+
+for day in days:
+    #plot for weekdays only "DayofWk < 5"
+    #plot for weekends only "DayofWk >= 5"
+    
+    dfTemp = dfEnergy.loc[dfEnergy.DayofYr == day];
+    dayTotal = len(dfTemp);
+    if dfTemp.DayofWk.iloc[0] >= 0:
+        for hr in hrs:
+            dfTempHr = dfTemp.loc[dfTemp.StartHr == hr];
+            arrivals = len(dfTempHr);
+            print(d,hr,arrivals)
+            arrivalCounts[d,hr] = arrivals;    
+            arrivalPcts[d,hr] = arrivals/dayTotal;    
+        d+=1;
+
+arrivalQuarts = np.zeros((24, 5));
+
+for hr in hrs:
+    #arrivalQuarts[hr] = [np.min(arrivalPcts[:,hr]), np.percentile(arrivalPcts[:,hr], 25, axis=0), np.percentile(arrivalPcts[:,hr], 50, axis=0), np.percentile(arrivalPcts[:,hr], 75, axis=0), np.max(arrivalPcts[:,hr])];
+    arrivalQuarts[hr] = [np.min(arrivalCounts[:,hr]), np.percentile(arrivalCounts[:,hr], 25, axis=0), np.percentile(arrivalCounts[:,hr], 50, axis=0), np.percentile(arrivalCounts[:,hr], 75, axis=0), np.max(arrivalCounts[:,hr])];
+
+#%% Plot Arrivals
+dfArrival = pd.DataFrame(arrivalQuarts, columns=['Min', '25pct', '50pct', '75pct', 'Max'])
+#dfArrival = dfArrival[['Max', '75pct', '50pct', '25pct', 'Min']
+
+colors = plt.cm.Blues(np.linspace(0,1,10))
+
+for i in range(5):
+    plt.plot(dfArrival.values[:,i], color=colors[2*i])
+
+plt.legend(['Min', '25pct', '50pct', '75pct', 'Max'])
+
+plt.xlabel('Hour of Day')
+plt.xticks(np.arange(0, 26, 2))
+plt.ylabel('EVs')
+plt.title('Arrivals Per Hour Quartiles')
+
+#%% Sparrow Math
+
+# Morning
+grp1 = dfSparrow[(dfSparrow.PluginHour <= 12) & (dfSparrow.Sparrow <= 0.5)]
+grp2 = dfSparrow[(dfSparrow.PluginHour <= 12) & (dfSparrow.Sparrow > 0.5)]
+#Afternoon
+grp3 = dfSparrow[(dfSparrow.PluginHour > 12) & (dfSparrow.Sparrow <= 0.5)]
+grp4 = dfSparrow[(dfSparrow.PluginHour > 12) & (dfSparrow.Sparrow > 0.5)]
