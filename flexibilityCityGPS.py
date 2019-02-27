e6e8a3b2fc90738a5692f75a8bb819f8e0dfefb1
apalom <35828580+apalom@users.noreply.github.com>
2019-02-27 10:42:28 -0700

    retrieve LatLon from Address

--- flexibilityCityGPS.py
+++ flexibilityCityGPS.py
@@ -7,13 +7,25 @@ Created on Mon Feb 25 14:07:45 2019
 
 from geopy import geocoders  
 import pandas as pd
+import googlemaps
+from datetime import datetime
 
 gn = geocoders.GeoNames(username='apalom')
+googleKey = 'AIzaSyBHTjbBLRMvxLEpgLQDan33Zrk3a04fa54'
+
 
 # Import Data
 filePath = 'data/UT-cities.csv';
 cities = pd.read_csv(filePath);
 
-print(gn.geocode("Cleveland, OH 44106"))
 
-location = gn.geocode("175 5th Avenue NYC")
+gmaps = googlemaps.Client(key=googleKey)
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+latlon = geocode_result[0]['geometry']['location']
+print(latlon)
+
+geocoded = []
+for address in cities['City']:  # assumes mydata is a pandas df
+    geocoded.append(googleGeo.geocode(address))  # geocode function returns a geocoded object
